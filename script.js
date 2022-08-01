240ee42d8418c0fc52504b509812f65b6b9bcc17
Mileide-Mikka <mi-leyde@hotmail.com>
2022-08-01 19:53:43 -0300

    Aqui fiz um conversor de distancia para treinar mais meus estudos sobre condicionais usando Switch... case.

---
+++ script.js
@@ -0,0 +1,30 @@
+//variavel que armazena a distanci
+let distanceLY = prompt("Qual a distância em Anos Luz?");
+
+let chosenOption = prompt("Para qual unidade devo converter?\n1. Parse(pc)\n2. unidade Astronômica" + "\n3. Quilômetros (Km)\n\n(Digite o numero da opção desejada)");
+
+let chosenUnity; // variavel para guarda a unidade escolhida pelo usuario
+let convertedDistance; // variavel para guarda a distancia convertida
+
+Switch(chosenOption) {
+    case "1":
+        chosenUnity = "Parsec";
+        convertedDistance = distanceLY * 0.306601;
+        break
+        
+    case "2":
+        chosenUnity = "Unidade Astronômica";
+        convertedDistance = distanceLY * 63241.1;
+        break
+
+    case "3":
+        chosenUnity = "Quilômetros";
+        convertedDistance = distanceLY * 9.5 * Math.pow(10, 12); //Math.pow vai converter o valor para conseguir sua elevação (numero elevado a x)
+        break
+
+    default:
+        chosenUnity = "Unidade não indentificada";
+        convertedDistance = "Conversão fora do padrão";
+}
+
+alert("A distância em Anos Luz: " + distanceLY + "\n" + chosenUnity + ":" + convertedDistance);
